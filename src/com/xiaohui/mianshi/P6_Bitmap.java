d6dc5e414504a0d6b0d3a3f990932636d5973887
linyuqiang <linyuqiang@100tal.com>
2021-07-25 21:37:15 +0800

    bitmap

---
+++ src/com/xiaohui/mianshi/P6_Bitmap.java
@@ -0,0 +1,40 @@
+package com.xiaohui.mianshi;
+
+public class P6_Bitmap {
+	private long[] words;
+	private int size;
+
+	public P6_Bitmap(int size) {
+		this.size = size;
+		words = new long[size];
+	}
+
+	public boolean getBit(int bitIndex) {
+		if (bitIndex < 0 || bitIndex > size - 1) {
+			throw new IndexOutOfBoundsException(bitIndex + " 超过bitmap长度 " + size);
+		}
+		int wordIndex = getWordIndex(bitIndex);
+		return (words[wordIndex] & (1L << bitIndex)) != 0;
+	}
+
+	public void setBit(int bitIndex) {
+		if (bitIndex < 0 || bitIndex > size - 1) {
+			throw new IndexOutOfBoundsException(bitIndex + " 超过bitmap长度 " + size);
+		}
+		int wordIndex = getWordIndex(bitIndex);
+		words[wordIndex] |= 1L << bitIndex;
+	}
+
+	private int getWordIndex(int bitIndex) {
+		return bitIndex >> 6;
+//		return bitIndex;
+	}
+
+	public static void main(String[] args) {
+		P6_Bitmap pBitmap = new P6_Bitmap(128);
+		pBitmap.setBit(126);
+		pBitmap.setBit(75);
+		System.out.println("getWordIndex:index1=" + pBitmap.getBit(126));
+		System.out.println("getWordIndex:index2=" + pBitmap.getBit(78));
+	}
+}
